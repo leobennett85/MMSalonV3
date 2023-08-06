e37557a48b0560056cc7721abab3042606f9103d
leobennett85 <leobennett85@mail.com>
2023-08-05 22:07:58 -0230

    Template for MMSalon created

---
+++ mmsalonv3/src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import './Home.css'
+
+const Home = () => {
+
+    return (
+      <>
+        <div id="home-wrap">
+          <div id="topbar-wrap">
+              topbar
+          </div>
+
+          <div id="navbar-wrap">
+            navbar
+          </div>
+
+          <div id="content-wrap">
+              <div id="colA-Booking-wrap">
+                  <div id="contentBox-wrap">
+                    contenbox
+                  </div>
+              </div>
+              <div id="colB-Booking-wrap">
+                video
+              </div>
+              <div id="colC-Booking-wrap">
+                book
+              </div>
+          </div>
+
+          <div id="bottombar-wrap">
+              bottombar
+          </div>
+        </div>
+      </>
+    );
+}
+
+export default Home;
